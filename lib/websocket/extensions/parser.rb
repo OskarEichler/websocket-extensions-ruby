8c11856725146cde90f61d075b179cc9b4037a59
Oskar Eichler <oskar@songstats.com>
2026-08-28 03:14:00 +0300

    Append duplicate parameter values without copying prior arrays

--- lib/websocket/extensions/parser.rb
+++ lib/websocket/extensions/parser.rb
@@ -43,7 +43,8 @@ module WebSocket
             end
 
             if offer.has_key?(key)
-              offer[key] = [*offer[key]] + [data]
+              offer[key] = [offer[key]] unless offer[key].is_a?(Array)
+              offer[key].push(data)
             else
               offer[key] = data
             end
